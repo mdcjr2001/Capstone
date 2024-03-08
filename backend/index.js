a797be24a4d5657cea2ac2f66f0b741cfce41337
Marcus Ceasor <145412305+mdcjr2001@users.noreply.github.com>
2024-03-07 19:47:19 -0600

    done

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,60 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const PORT = 8000
+const cors = require('cors');
 
-// routes
-const userRoutes = require('./routes/userRoutes')
+
+// require("dotenv").config();
+// parse requests of content-type - application / json;
+
+let dbConnect = require("./dbConnect")
+let userRoutes = require('./routes/userRoutes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
 
 app.use(express.json());
 
-// defining end points
-app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes);
 
-app.use('/', (req, res) => {
-    res.send({ status: 200, message: "Welcome to Capstone Page" })
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my MongoDB application." });
 });
 
+
+app.post("/#",async(req,res)=>{
+    const{email,password}=req.body
+
+    const data={
+        email:email,
+        password:password
+    }
+
+    try{
+        const check=await collection.findOne({email:email})
+
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("notexist")
+            await collection.insertMany([data])
+        }
+
+    }
+    catch(e){
+        res.json("fail")
+    }
+
+})
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8001;
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`)
+  console.log(`Server is running on port
+${PORT}.`);
 });
